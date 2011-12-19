e3fc90fdeedd0b7945617ef1e75b6fd67c04283e
Anders Lövgren <andlov@nowise.se>
2011-12-19 16:34:33 +0000

    The remote host properties were previously handled by an internal class in the service handler. It is now moved to its own source file to keep thing clean.

---
+++ whoson-asp/RemoteHost.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web;
+using System.Net;
+
+namespace WhosOn.ASP
+{
+    public class RemoteHost
+    {
+        private HttpRequest request;
+        private IPHostEntry hostent;
+
+        public RemoteHost(HttpRequest request)
+        {
+            this.request = request;
+            this.hostent = Dns.GetHostEntry(request.UserHostAddress);
+        }
+
+        public string Address
+        {
+            get { return hostent.AddressList[0].ToString(); }
+        }
+
+        public string HostName
+        {
+            get { return hostent.HostName; }
+        }
+
+        public HttpRequest Request
+        {
+            get { return request; }
+        }
+    }
+}
