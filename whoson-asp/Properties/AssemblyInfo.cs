926e01deed2a370cbbfe835d4e5e5abddce79465
Anders Lövgren <andlov@nowise.se>
2012-05-08 22:28:24 +0000

    Incremented project version. Fixed copyright years.

--- whoson-asp/Properties/AssemblyInfo.cs
+++ whoson-asp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Uppsala University")]
 [assembly: AssemblyProduct("whoson_asp")]
-[assembly: AssemblyCopyright("Copyright © 2011 Anders Lövgren (QNET/Compdept BMC)")]
+[assembly: AssemblyCopyright("Copyright © 2011-2012 Anders Lövgren (QNET/Compdept BMC)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyFileVersion("1.2.0.1")]
